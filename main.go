974e34f68d7eb545ad3393f9c9858e282461fe3c
agrimmer <andreas.grimmer@dynatrace.com>
2020-10-03 13:06:34 +0200

    Graceful shutdown

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type myHandler struct {
+}
+
+func (myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	fmt.Fprintf(w, "Good day, today we have %s and it is %02d:%02d", t.Weekday().String(), t.Hour(), getMinute(t.Minute(), t.Second()))
+	fmt.Fprintf(w, "Good day, it is %02d:%02d", t.Hour(), getMinute(t.Minute(), t.Second()))
 }
 
 func getMinute(minute int, second int) int {
@@ -17,6 +24,43 @@ func getMinute(minute int, second int) int {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+
+	srv := &http.Server{
+		Addr:         ":8888",
+		Handler:      &myHandler{},
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+
+		// interrupt signal sent from terminal
+		signal.Notify(sigint, os.Interrupt)
+		// sigterm signal sent from kubernetes
+		signal.Notify(sigint, syscall.SIGTERM)
+
+		<-sigint
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer func() {
+			// extra handling here
+			cancel()
+		}()
+
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(ctx); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		// Error starting or closing listener:
+		log.Printf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
 }
