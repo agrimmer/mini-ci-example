2d5959c8281692da3f71ff4ae13f08e50391e0ce
agrimmer <andreas.grimmer@dynatrace.com>
2020-10-03 09:15:45 +0200

    Correct formatting

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	fmt.Fprintf(w, "Good day, it is %d:%d", t.Hour(), getMinute(t.Minute(), t.Second()))
+	fmt.Fprintf(w, "Good day, it is %02d:%02d", t.Hour(), getMinute(t.Minute(), t.Second()))
 }
 
 func getMinute(minute int, second int) int {
